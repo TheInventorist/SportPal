08e5a0ab48661ffee8f22189d2e909c1d1887cf4
AntoBueno <antonellabueno25@gmail.com>
2020-04-12 18:14:28 -0400

    union vista login y registro

--- MyPal/app/src/main/java/com/example/mypal/MainActivity.java
+++ MyPal/app/src/main/java/com/example/mypal/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.mypal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v){
+        Intent myIntent = new Intent(getBaseContext(), registro.class);
+        startActivity(myIntent);
     }
 }
