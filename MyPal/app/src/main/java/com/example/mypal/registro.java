c0b802aacb66978c1e0707624841fdeb08ba17d7
AntoBueno <antonellabueno25@gmail.com>
2020-04-12 20:04:20 -0400

    actualizacion de logo, mockups y vistas

--- MyPal/app/src/main/java/com/example/mypal/registro.java
+++ MyPal/app/src/main/java/com/example/mypal/registro.java
@@ -1,8 +1,9 @@
 package com.example.mypal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registro extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+    public void onButtonVerif(View v){
+        Intent myIntent2 = new Intent(getBaseContext(), verificador.class);
+        startActivity(myIntent2);
     }
 }
