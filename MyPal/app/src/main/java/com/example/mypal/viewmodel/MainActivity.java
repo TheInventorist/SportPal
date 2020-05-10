6a444ada948b5b7a7a3410c653f42666dc3964a4
Vincent <vdepassier@gmail.com>
2020-05-10 19:46:56 -0400

    login facebook firebase

--- MyPal/app/src/main/java/com/example/mypal/viewmodel/MainActivity.java
+++ MyPal/app/src/main/java/com/example/mypal/viewmodel/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mypal.viewmodel;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -13,22 +14,38 @@ import android.database.sqlite.SQLiteDatabase;
 import android.Manifest;
 import android.content.pm.PackageManager;
 
+import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mypal.R;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
 
 import android.os.Bundle;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -38,6 +55,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
  private FirebaseDatabase fireBaseDataBase;
  private DatabaseReference dataBaseReference;
  private FirebaseAuth mAuth;
+ private FirebaseAuth.AuthStateListener authStateListener;
+
+ private CallbackManager mCallBackManager;
+ private LoginButton botonLoginFacebook;
+ private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +72,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btnCrearcuenta).setOnClickListener(this);
 
         inicializarFireBase();
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+
+        botonLoginFacebook = findViewById(R.id.login_button_fb);
+        botonLoginFacebook.setReadPermissions("email","public_profile");
+        mCallBackManager = CallbackManager.Factory.create();
+
+
+        botonLoginFacebook.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!= null){
+                    Intent myIntent4 = new Intent(getBaseContext(), PantallaCarga.class);
+                    startActivity(myIntent4);
+                }else{
+                    // stay there dud
+                }
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if(currentAccessToken == null){
+                    mAuth.signOut();
+                }
+            }
+        };
 
 
         //Implementacion SQLite
@@ -73,12 +144,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         */
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+
+
+
 
     }
 
+    protected void handleFacebookToken(AccessToken token){
+        AuthCredential credential = FacebookAuthProvider.getCredential((token.getToken()));
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent myIntent4 = new Intent(getBaseContext(), PantallaCarga.class);
+                    startActivity(myIntent4);
 
+                }else{
+                    Toast.makeText(MainActivity.this, "Fallo la Autenticacion", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    /*
     @Override
     protected void onStart() {
         super.onStart();
@@ -87,7 +181,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(this, MainActivity.class));
         }
     }
-
+*/
 
 
     @Override
